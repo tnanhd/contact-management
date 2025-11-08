37a5d64b171e0825f79e3a2ff448259483d5412c
tnanhd <tnanh.dev@gmail.com>
2025-11-09 01:47:19 +0700

    docs: add swagger

--- src/main/java/com/example/contact/adapter/in/rest/ping/PingController.java
+++ src/main/java/com/example/contact/adapter/in/rest/ping/PingController.java
@@ -1,15 +1,17 @@
 package com.example.contact.adapter.in.rest.ping;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Ping Controller", description = "Endpoints to check if the application is running")
 @RequestMapping("/api/v1/ping")
 public class PingController {
 
   @GetMapping()
-  public String checkHealth() {
+  public String ping() {
     return "Pong! The application is running!";
   }
 }
--- src/main/java/com/example/contact/adapter/in/rest/user/controller/UserManagementController.java
+++ src/main/java/com/example/contact/adapter/in/rest/user/controller/UserManagementController.java
@@ -3,6 +3,7 @@ package com.example.contact.adapter.in.rest.user.controller;
 import com.example.contact.adapter.in.rest.user.mapper.UserDetailsMapper;
 import com.example.contact.adapter.in.rest.user.dto.response.UserDetails;
 import com.example.contact.domain.port.in.user.GetUsersUseCase;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/users")
+@Tag(name = "User Management Controller", description = "Endpoints for managing users")
 @RequiredArgsConstructor
 public class UserManagementController {
 
