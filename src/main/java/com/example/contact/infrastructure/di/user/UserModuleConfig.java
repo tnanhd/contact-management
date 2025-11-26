c58e59bbc160cc95c8263b61bbe7968b002bbd9d
tnanhd <tnanh.dev@gmail.com>
2025-11-26 08:31:23 +0700

    ref: minor change

--- src/main/java/com/example/contact/infrastructure/di/user/UserModuleConfig.java
+++ src/main/java/com/example/contact/infrastructure/di/user/UserModuleConfig.java
@@ -1,5 +1,6 @@
 package com.example.contact.infrastructure.di.user;
 
+import com.example.contact.domain.port.in.user.GetUsersUseCase;
 import com.example.contact.domain.usecase.user.GetUsersUseCaseImpl;
 import com.example.contact.domain.port.out.UserRepository;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class UserModuleConfig {
 
   @Bean
-  public GetUsersUseCaseImpl getUsersUseCase(UserRepository userRepository) {
+  public GetUsersUseCase getUsersUseCase(UserRepository userRepository) {
     return new GetUsersUseCaseImpl(userRepository);
   }
 }
